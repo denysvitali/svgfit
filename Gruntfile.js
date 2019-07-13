94da7e41a7e2029a9378220f1d7ce149dd63f1ad
Denys Vitali <denys@denv.it>
2019-07-13 14:56:51 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    svgfit: {
+      files: 
+        {
+          src: ['svg/*.svg'], 
+          dest: 'output/'
+        }
+      
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-svgfit');
+  grunt.registerTask('default', ['svgfit']);
+};
